efaa8de72ed052ec1113d780cb754974b4752431
Vincent Uiker <uiker.vincent@gmail.com>
2021-05-05 23:25:03 +0200

    added TurnSpec

---
+++ src/test/scala/de/htwg/se/settlers/model/TurnSpec.scala
@@ -0,0 +1,21 @@
+package de.htwg.se.settlers.model
+
+import de.htwg.se.settlers.model.Game.PlayerID
+import org.scalatest.{Matchers, WordSpec}
+
+class TurnSpec extends WordSpec with Matchers {
+  "Turn" when {
+    val turn = Turn( new PlayerID( 1 ) )
+    "new" should {
+      "have playerID" in {
+        turn.playerID.id shouldBe 1
+      }
+      "manage dev cards" in {
+        val turn2 = turn.addDrawnDevCard( KnightCard )
+        turn2.drawnDevCards shouldBe List( KnightCard )
+        turn2.removeDrawnDevCard().drawnDevCards shouldBe empty
+        turn2.getLastDrawnDevCard shouldBe Some( KnightCard )
+      }
+    }
+  }
+}
