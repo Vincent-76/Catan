c9981a2c0e822cf17cbb96ae7feda8b1c3cec074
Vincent Uiker <uiker.vincent@gmail.com>
2021-05-05 23:21:38 +0200

    added ResourceSpec

---
+++ src/test/scala/de/htwg/se/settlers/model/ResourceSpec.scala
@@ -0,0 +1,19 @@
+package de.htwg.se.settlers.model
+
+import org.scalatest.{Matchers, WordSpec}
+
+class ResourceSpec extends WordSpec with Matchers{
+  "Resources" when {
+    "static" should {
+      "be constructed with of" in {
+        Resources.of( "wOoD" ) shouldBe Some( Wood )
+        Resources.of( "Woo" ) shouldBe None
+      }
+    }
+    "new" should {
+      "have a string representation" in {
+        Wood.toString shouldBe Wood.title
+      }
+    }
+  }
+}
