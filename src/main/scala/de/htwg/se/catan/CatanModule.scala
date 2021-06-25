2b8a364d0b67d87e83a3749dccf66e0cb861760f
Vincent Uiker <uiker.vincent@gmail.com>
2021-06-25 20:21:26 +0200

    some changes

--- src/main/scala/de/htwg/se/catan/CatanModule.scala
+++ src/main/scala/de/htwg/se/catan/CatanModule.scala
@@ -5,41 +5,20 @@ import com.google.inject.assistedinject.FactoryModuleBuilder
 import com.google.inject.name.Names
 import de.htwg.se.catan.controller.Controller
 import de.htwg.se.catan.controller.controllerBaseImpl.ClassicControllerImpl
+import de.htwg.se.catan.model._
+import de.htwg.se.catan.model.impl.fileio.JsonFileIO
 import de.htwg.se.catan.model.impl.game.ClassicGameImpl
 import de.htwg.se.catan.model.impl.gamefield.ClassicGameFieldImpl
 import de.htwg.se.catan.model.impl.placement.{ CityPlacement, RoadPlacement, RobberPlacement, SettlementPlacement }
 import de.htwg.se.catan.model.impl.player.ClassicPlayerImpl
 import de.htwg.se.catan.model.impl.turn.ClassicTurnImpl
-import de.htwg.se.catan.model._
-import de.htwg.se.catan.model.impl.fileio.{ JsonFileIO, JsonParseError, XMLFileIO, XMLParseError }
 import de.htwg.se.catan.model.state._
 import net.codingwell.scalaguice.ScalaModule
-import play.api.libs.json.{ JsValue, Json }
 
 import scala.util.Random
-import scala.xml.Node
 
 object CatanModule {
-
-  /*def gameFromXML( node:Node ):Game = node.label match {
-    case "ClassicGameImpl" => ClassicGameImpl.fromXML( node )
-    case e => throw XMLParseError( expected = "Game", got = e )
-  }
-
-  def gameFieldFromXML( node:Node ):GameField = node.label match {
-    case "ClassicGameFieldImpl" => ClassicGameFieldImpl.fromXML( node )
-    case e => throw XMLParseError( expected = "GameField", got = e )
-  }
-
-  def turnFromXML( node:Node ):Turn = node.label match {
-    case "ClassicTurnImpl" => ClassicTurnImpl.fromXML( node )
-    case e => throw XMLParseError( expected = "Turn", got = e )
-  }
-
-  def playerFromXML( node:Node ):Player = node.label match {
-    case "ClassicPlayerImpl" => ClassicPlayerImpl.fromXML( node )
-    case e => throw XMLParseError( expected = "Player", got = e )
-  }*/
+  val savegamePath:String = "savegames"
 
   def playerFactoryFromString( playerFactoryClass:String ):Option[PlayerFactory] = playerFactoryClass match {
     case "ClassicPlayerImpl" => Some( ( pID:PlayerID, color:PlayerColor, name:String ) => ClassicPlayerImpl( pID, color, name ) )
--- src/main/scala/de/htwg/se/catan/model/FileIO.scala
+++ src/main/scala/de/htwg/se/catan/model/FileIO.scala
@@ -1,11 +1,16 @@
 package de.htwg.se.catan.model
 
+import de.htwg.se.catan.CatanModule
+
 import java.text.SimpleDateFormat
 import java.util.Calendar
+import scala.reflect.io.File
 
 trait FileIO {
-  def getFileName:String =
+  def getFileName:String = {
+    File( CatanModule.savegamePath ).createDirectory().path + File.separator +
     "Catan_" + new SimpleDateFormat( "YYYY-MM-dd_HH.mm.ss" ).format( Calendar.getInstance().getTime ) + "_savegame"
+  }
 
   def load( path:String ):Game
 
